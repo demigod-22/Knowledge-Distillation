844305f51f0bea6e5e55a3a5b86569835f35d02b
ArkaRutvik <96034045+demigod-22@users.noreply.github.com>
2023-06-01 18:05:58 +0530

    Add files via upload

---
+++ Student Models/Ensemble.py
@@ -0,0 +1,37 @@
+
+from Student Model import *
+
+
+class Ensemble(nn.Module):
+
+    def __init__(self, modelA, modelB, modelC, input):
+        super(Ensemble, self).__init__()
+        self.modelA = modelA
+        self.modelB = modelB
+        self.modelC = modelC
+
+        self.fc1 = nn.Linear(input, 16)
+
+    def forward(self, x):
+        out1 = self.modelA(x)
+        out2 = self.modelB(x)
+        out3 = self.modelC(x)
+
+        out = out1 + out2 + out3
+
+        x = self.fc1(out)
+        return torch.softmax(x, dim=1)
+    
+
+model = Ensemble(net_st1, net_st2 , net_st3, 16)
+
+
+model.to(device)
+
+
+optimizer = optim.Adam(model.parameters(),lr=0.003)
+
+
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+model, train_loss, test_loss = Helper.train(model, train_loader, test_loader, epoch, optimizer, criterion)
